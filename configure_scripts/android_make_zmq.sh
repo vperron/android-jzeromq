594226377c0b76bf8e3eed6caab19896c574cb2c
Claude Mamo <claude.mamo@ricston.com>
2012-08-12 10:01:47 +0200

    Added includes for Android NDK R8b

--- configure_scripts/android_make_zmq.sh
+++ configure_scripts/android_make_zmq.sh
@@ -5,9 +5,9 @@ lib_dir=$INSTALL_PATH/lib
 tools_prefix=$ARM_TOOLCHAIN/bin/arm-linux-androideabi
 
 if [ ! -f .is_configured ]; then \
-	LD_LIBRARY_PATH=$lib_dir  \
+	LD_LIBRARY_PATH=$lib_dir \
 	CFLAGS="-fPIC -I$include_dir -I$ARM_TOOLCHAIN/arm-none-linux-gnueabi/include -Wl,-rpath=$ARM_TOOLCHAIN/arm-none-linux-gnueabi/lib" \
-	CPPFLAGS="-I$include_dir -fvisibility=default" \
+	CPPFLAGS="-I$include_dir -fvisibility=default -I$ARM_TOOLCHAIN/arm-linux-androideabi/include/c++/4.6 -I$ARM_TOOLCHAIN/arm-linux-androideabi/include/c++/4.6/arm-linux-androideabi" \
 	LDFLAGS="-L$lib_dir -lgcc -Wl,-rpath=$lib_dir -Wl,-Bstatic -luuid -Wl,-Bdynamic" \
 	CC=$tools_prefix-gcc \
 	CXX=$tools_prefix-g++ \
